50f0eeb421fdd5000502f1e6ca60bf5470ab9570
Lilii <lilii-ann.older@voco.ee>
2023-06-06 11:34:09 +0300

    Use dynamic CSS modules for CourseInput

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
@@ -47,10 +47,10 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl invalid={!isValid}>
+            <div className={`${styles['formControl']} ${!isValid && styles.invalid}`}>
                 <label>Course Goal</label>
-                <input type="text" onChange={goalInputChangeHandler} />
-            </FormControl>
+                <input type={'text'} onChange={goalInputChangeHandler} />
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
